fa12c3eb5767db617f42a4e67efa231aef014906
Burak Ülkü <brkulku@gmail.com>
2023-09-11 22:17:50 +0200

    Create Random Color React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import useGenerateRandomColor from './useGenerateRandomColor';
+
 export default function App() {
+  const { color, generateColor } = useGenerateRandomColor();
   return (
-    <>
-      <h1>Random Color Generator</h1>
-      <div style={{ fontSize: '100px' }}>
-        <button>+</button>
-        <button>-</button>
-      </div>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#' + color,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <button
+        style={{
+          padding: '40px',
+          borderRadius: '10px',
+          backgroundImage: 'linear-gradient(to top, #a8edea 0%, #fed6e3 100%)',
+          fontSize: 'larger',
+        }}
+        onClick={generateColor}
+      >
+        Generate random color
+      </button>
+    </div>
   );
 }
